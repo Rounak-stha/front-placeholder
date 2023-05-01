64973b7e624d9272467c465f3cc2a0d73960e41f
Rouak-stha <rounakshrestha1153@gmail.com>
2023-05-01 17:19:34 +0545

    Use Local Images

--- src/app/api/shop/route.ts
+++ src/app/api/shop/route.ts
@@ -5,44 +5,44 @@ const shopData: ShopItem[] = [
 	{
 		id: 'abcde',
 		name: 'MacBook',
-		image: 'https://images.unsplash.com/photo-1588053631229-d04fd47cf0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFjc2V0dXB8ZW58MHx8MHx8&w=1000&q=80',
+		image: '/images/mac_dark.png',
 		price: '$1200',
 		count: 5
 	},
 	{
 		id: 'abcdf',
 		name: 'MacBook',
-		image: 'https://images.unsplash.com/photo-1588053631229-d04fd47cf0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFjc2V0dXB8ZW58MHx8MHx8&w=1000&q=80',
-		price: '$1200',
-		count: 5
+		image: '/images/mac.png',
+		price: '$1999',
+		count: 3
 	},
 	{
 		id: 'abcdg',
-		name: 'MacBook',
-		image: 'https://images.unsplash.com/photo-1588053631229-d04fd47cf0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFjc2V0dXB8ZW58MHx8MHx8&w=1000&q=80',
-		price: '$1200',
-		count: 5
+		name: 'Iphone',
+		image: '/images/iphone.png',
+		price: '$1099',
+		count: 9
 	},
 	{
 		id: 'abcdh',
 		name: 'MacBook',
-		image: 'https://images.unsplash.com/photo-1588053631229-d04fd47cf0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFjc2V0dXB8ZW58MHx8MHx8&w=1000&q=80',
+		image: '/images/mac_dark.png',
 		price: '$1200',
 		count: 5
 	},
 	{
 		id: 'abcdi',
 		name: 'MacBook',
-		image: 'https://images.unsplash.com/photo-1588053631229-d04fd47cf0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFjc2V0dXB8ZW58MHx8MHx8&w=1000&q=80',
-		price: '$1200',
-		count: 5
+		image: '/images/mac.png',
+		price: '$1999',
+		count: 3
 	},
 	{
 		id: 'abcdj',
-		name: 'MacBook',
-		image: 'https://images.unsplash.com/photo-1588053631229-d04fd47cf0ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFjc2V0dXB8ZW58MHx8MHx8&w=1000&q=80',
-		price: '$1200',
-		count: 5
+		name: 'Iphone',
+		image: `/images/iphone.png`,
+		price: '$1099',
+		count: 9
 	}
 ]
 
