67ebadcf6c06aa9e6c15b31104edce1296cba805
Rouak-stha <rounakshrestha1153@gmail.com>
2023-05-05 14:08:31 +0545

    Granular Use Selector

--- src/app/redux/shop/components/ShopItem.tsx
+++ src/app/redux/shop/components/ShopItem.tsx
@@ -4,19 +4,20 @@ import ToggleFromCartBtn from './ToggleFromCartBtn'
 import ShopItemCount from './ShopItemCount'
 
 export default function ShopItem({ SItem }: { SItem: SI }) {
-	const { id, name, image, price, count } = SItem
+	const { id, name, image, price, currency, count } = SItem
 	return (
-		<div className='border border-gray-800'>
+		<div className='border border-bdr-light'>
 			<div className='relative h-40'>
 				<Image className='rounded-sm mb-2' alt='Macbook' src={image} fill style={{ objectFit: 'cover' }} />
 			</div>
 			<div className='m-3'>
 				<div className='flex mb-1 font-bold'>
 					<p className='flex-1 '>{name}</p>
-					<p className='text-sm'>{price}</p>
+					<p className='text-sm'>{currency + price}</p>
 				</div>
 				<div className='flex items-center'>
-					<ShopItemCount itemId={id} count={count} /> <ToggleFromCartBtn shopItem={SItem} />
+					<ShopItemCount itemId={id} count={count} />
+					<ToggleFromCartBtn shopItemId={id} />
 				</div>
 			</div>
 		</div>
--- src/app/redux/shop/components/ToggleFromCartBtn.tsx
+++ src/app/redux/shop/components/ToggleFromCartBtn.tsx
@@ -6,43 +6,48 @@ import {
 	incrementCartItemCount,
 	removeCartItem
 } from '../../features/cart/cartSlice'
-import { ShopItem } from '../../features/shop/shopSlice'
 import { useAppDispatch, useAppSelector } from '@/hooks'
 
-export default function ToggleFromCartBtn({ shopItem }: { shopItem: ShopItem }) {
+export default function ToggleFromCartBtn({ shopItemId }: { shopItemId: string }) {
 	const cartItemCount = useAppSelector((state) => {
-		const foundItem = state.cart.items.find((i) => i.id === shopItem.id)
+		const foundItem = state.cart.items.find((i) => i.id === shopItemId)
 		if (foundItem) return foundItem.count
 		else return 0
 	})
 
+	const shopItem = useAppSelector((state) => state.shop.items.find((i) => i.id === shopItemId))
+
+	if (!shopItem) throw 'No Shop Item'
+
+	const shopItemCount = shopItem.count
+
 	const dispatch = useAppDispatch()
 
 	function increaseCartItem() {
-		if (cartItemCount === 0) dispatch(addCartItem({ ...shopItem, count: cartItemCount + 1 }))
-		else dispatch(incrementCartItemCount(shopItem.id))
+		if (cartItemCount === 0) dispatch(addCartItem({ ...shopItem!, count: cartItemCount + 1 }))
+		else dispatch(incrementCartItemCount(shopItemId))
 	}
 
 	function decreaseCartItem() {
-		if (cartItemCount === 1) dispatch(removeCartItem(shopItem.id))
-		else dispatch(decrementCartItemCount(shopItem.id))
+		if (cartItemCount === 1) dispatch(removeCartItem(shopItemId))
+		else dispatch(decrementCartItemCount(shopItemId))
 	}
 	return (
-		<div className='flex items-center font-bold rounded-sm border-2 border-gray-600'>
+		<div className='flex items-center w-fit font-bold rounded-sm border-2 border-bdr-light'>
 			<button
 				onClick={() => decreaseCartItem()}
-				className='px-2 py-0.5 border-r-2 border-r-gray-600'
+				className='px-3 py-0.5 border-r-2 border-r-bdr-light disabled:cursor-not-allowed'
 				disabled={cartItemCount === 0}
 				aria-disabled={cartItemCount === 0}
 			>
 				-
 			</button>
-			<p className='px-2 py-0.5 text-sm'>{cartItemCount}</p>
+			<p className='px-3 py-0.5 text-sm'>{cartItemCount}</p>
 			<button
 				onClick={() => increaseCartItem()}
-				className='px-2 py-0.5 border-l-2 border-l-gray-600'
-				disabled={cartItemCount === shopItem.count}
-				aria-disabled={cartItemCount === shopItem.count}
+				className='px-3 py-0.5 border-l-2 border-l-bdr-light disabled:cursor-not-allowed'
+				disabled={cartItemCount === shopItemCount}
+				aria-disabled={cartItemCount === shopItemCount}
 			>
 				+
 			</button>
