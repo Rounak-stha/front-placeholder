1ec1a0f9a9b7d2005eb4a66f78ea289fec60d22a
Rouak-stha <rounakshrestha1153@gmail.com>
2023-05-05 13:51:34 +0545

    Used New Color Tokens

--- src/ui/Container.tsx
+++ src/ui/Container.tsx
@@ -2,5 +2,5 @@ interface Props {
 	children: React.ReactNode
 }
 export default function Container({ children }: Props) {
-	return <div className='p-4 border border-gray-800 rounded-md'>{children}</div>
+	return <div className='p-4 border border-bdr-light rounded-md'>{children}</div>
 }
